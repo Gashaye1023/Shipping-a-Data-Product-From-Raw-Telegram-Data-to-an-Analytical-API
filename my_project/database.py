105bbbe2ddac2c55a8643788a485cbb98ad27ff5
Gashaye1023 <gasha.como@gmail.com>
2025-07-25 16:06:02 +0300

    f

---
+++ my_project/database.py
@@ -0,0 +1,24 @@
+import psycopg2
+from contextlib import contextmanager
+
+def get_db_connection():
+    conn = psycopg2.connect(
+        host="localhost",
+        port="5432",
+        database="Shipping_dbt", # Ensure this matches your 
+        user="postgres",
+        password="1024" 
+    )
+    return conn
+
+@contextmanager
+def get_db():
+    conn = get_db_connection()
+    try:
+        yield conn
+        conn.commit()
+    except Exception as e:
+        conn.rollback()
+        raise e
+    finally:
+        conn.close()
